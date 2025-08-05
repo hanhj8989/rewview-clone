0572a7ecc5ed4bf15bfddb6cd7a63160ecf8587b
SeongTaekPark <esulove81@gmail.com>
2025-08-05 16:00:12 +0900

    프로젝트 설정

--- app/src/main/java/kr/co/rewview/clone/MainActivity.kt
+++ app/src/main/java/kr/co/rewview/clone/MainActivity.kt
@@ -2,11 +2,6 @@ package kr.co.rewview.clone
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,17 +11,7 @@ import kr.co.rewview.clone.ui.theme.ReviewCloneTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ReviewCloneTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
     }
 }
 
