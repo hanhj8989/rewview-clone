e2c4bc82299f36c3958ce4a512b9e9ed9b957d59
HHJ89 <89hhj0602@gmail.com>
2025-09-12 18:08:42 +0900

    출금하기 화면

---
+++ app/src/main/java/kr/co/rewview/clone/ui/activity/CashUseActivity.kt
@@ -0,0 +1,14 @@
+package kr.co.rewview.clone.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.rewview.clone.R
+
+class CashUseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_cash_use)
+    }
+
+}
--- app/src/main/java/kr/co/rewview/clone/ui/fragment/MyPageFragment.kt
+++ app/src/main/java/kr/co/rewview/clone/ui/fragment/MyPageFragment.kt
@@ -1,11 +1,13 @@
 package kr.co.rewview.clone.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kr.co.rewview.clone.R
+import kr.co.rewview.clone.ui.activity.CashUseActivity
 
 class MyPageFragment : Fragment() {
 
@@ -15,6 +17,12 @@ class MyPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_mypage, container, false)
+        
+        view.findViewById<View>(R.id.button_cash_use).setOnClickListener {
+            val intent = Intent(requireContext(), CashUseActivity::class.java)
+            startActivity(intent)
+        }
+        
         return view
     }
 }
